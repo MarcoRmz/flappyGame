709c543a2852c4f281de88dd6b9265b3414eb223
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-03-12 11:37:21 -0600

    comentarios juego

--- src/flappy/Juego.java
+++ src/flappy/Juego.java
@@ -45,36 +45,38 @@ public class Juego extends JFrame implements Runnable, KeyListener,
     private Animacion animaUsb, animaUsb2;
 
     //constantes de dificultad por nivel
-    private static final float velSalto = 7.5f;
-    private static final float gravedad = .55f;
-    private static final int separacionColumnas = 250;
-    private static final int separacion = 220;
+    private static final float velSalto = 7.5f; //velocidad en y al saltar
+    private static final float gravedad = .55f; //cambio de velocidad en y por frame
+    private static final float velUsb = 4.5f; //velocidad de los tubos en x
+    private static final int separacion = 220; //separacion entre el tubo de arriba y abajo
 
     //Variables de control de tiempo de la animación
     private long tiempoActual;
 
     private static final long serialVersionUID = 1L;
-    private static final int VIVO = 0, MUERTO = 1, NO_EMPEZADO = 2, PAUSA = 4, INSTRUCCIONES = 8;
+    private static final int VIVO = 0, MUERTO = 1, NO_EMPEZADO = 2, PAUSA = 4; //posibles estados del juego
 
     private Image dbImage;	// Imagen a proyectar
     private Image BKG;  //Imagen de fondo
     private Graphics dbg;	// Objeto grafico
-    private SoundClip sad;    // Objeto AudioClip
-    private SoundClip joy; //Obbjeto AudioClip
 
     private Bird bird;    // Objeto de la clase bola
-    private LinkedList<Usb> arriba, abajo;
+    private LinkedList<Usb> arriba, abajo; //los tubos de arriba y abajo
 
-    private boolean salta, restart;
-    private int score;
-    private int state;
-    private int nivel;
+    private boolean salta, restart; //indica si salta el pajaro y si se restartea el juego
+    private int score; //score del jugador
+    private int state; //estado del juego
+    private int nivel; //nivel de dificultad
     private String nombre; //nombre del jugador
 
-    private float getVelUsb(int nivel) {
-        return (float) (3 + nivel);
+    //regresa la separacion de las columnas en funcion del nivel de dificultad
+    private int getSeparacionColumnas(int nivel) {
+        if(nivel >= 4) //despues de nivel 4 ya no se juntan mas porque no se puede
+            return 120;
+        return 280 - nivel*40;
     }
 
+    //regresa la minima posicion en y del tubo en funcion del nivel de dificultad
     private int getMin(int nivel) {
         if (nivel > 20) {
             return 0;
@@ -82,6 +84,7 @@ public class Juego extends JFrame implements Runnable, KeyListener,
         return 200 - nivel * 10;
     }
 
+    //regresa la maxima posicion en y del tubo en funcion del nivel de dificultad
     private int getMax(int nivel) {
         if (nivel > 20) {
             return 800;
@@ -96,21 +99,20 @@ public class Juego extends JFrame implements Runnable, KeyListener,
      */
     public void init() {
         setSize(480, 800);  //Declara el tamaño del applet
+        
+        //carga el background
         BKG = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("background.png"));
 
         addKeyListener(this);
         addMouseListener(this);
         addMouseMotionListener(this);
 
-        /*//Se cargan los sonidos.
-         sad = new SoundClip("sad.wav");
-         joy = new SoundClip("joy.wav");*/
         //Se cargan las imágenes(cuadros) para la animación de bola y canasta
         Image bird1 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("bird1.png"));
         Image usb1 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("usb.png"));
         Image usb2 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("usb2.png"));
 
-        //Se crea la animación de bola
+        //Se crea la animación del parajaro y del usb
         animaBird = new Animacion();
         animaBird.sumaCuadro(bird1, 150);
         animaUsb = new Animacion();
@@ -118,37 +120,45 @@ public class Juego extends JFrame implements Runnable, KeyListener,
         animaUsb2 = new Animacion();
         animaUsb2.sumaCuadro(usb2, 150);
 
-        startGame();
+        startGame(); //inicializa el juego
     }
 
     private void startGame() {
 
-        //Crea personaje bola
+        //Crea personaje bird y lo pone en el centro
         bird = new Bird(getWidth() / 2, getHeight() / 2, animaBird);
         bird.setPosX(bird.getPosX() - bird.getAncho() / 2);
         bird.setPosY(bird.getPosY() - bird.getAlto() / 2);
         bird.setAccelY(gravedad);
 
+        //crea las listas de tubos
         arriba = new LinkedList<>();
         abajo = new LinkedList<>();
 
+        //inicializa las variables del juego
         nivel = 0;
         score = 0;
         state = NO_EMPEZADO; //pone el estado actual a no empezado
 
-        insertaUsbs(separacionColumnas * 3);
-        insertaUsbs(separacionColumnas * 4);
-        insertaUsbs(separacionColumnas * 5);
+        //inserta 3 tubos
+        insertaUsbs(getSeparacionColumnas(nivel) * 3);
+        insertaUsbs(getSeparacionColumnas(nivel) * 4);
+        insertaUsbs(getSeparacionColumnas(nivel) * 5);
 
     }
 
+    //inserta un tubo en la lista de tubos dada la posicion en x
     private void insertaUsbs(int x) {
         Random r = new Random();
+        //se selecciona al azar el punto medio entre los tubos en base al minimo y al maximo
         int puntoMedio = r.nextInt(getMax(nivel) - separacion - getMin(nivel)) + getMin(nivel) + separacion / 2;
+        
+        //agrega tubos a la lista de tubos
         arriba.addLast(new Usb(x, puntoMedio - separacion / 2 - 800, animaUsb2));
         abajo.addLast(new Usb(x, puntoMedio + separacion / 2, animaUsb));
-        arriba.peekLast().setVelX(-getVelUsb(nivel));
-        abajo.peekLast().setVelX(-getVelUsb(nivel));
+        //les pone velocidad en x a los tubos
+        arriba.peekLast().setVelX(-velUsb);
+        abajo.peekLast().setVelX(-velUsb);
     }
 
     /**
@@ -176,7 +186,7 @@ public class Juego extends JFrame implements Runnable, KeyListener,
         tiempoActual = System.currentTimeMillis();
 
         while (true) {
-            //Checa si la variable bool pausa o instrucciones o guarda es diferente a true para detener el juego
+            //Checa si la variable bool pausa para que no se actualize el juego
             if (state < 3) {
                 actualiza();
                 checaColision();
@@ -205,39 +215,39 @@ public class Juego extends JFrame implements Runnable, KeyListener,
         //Guarda el tiempo actual
         tiempoActual += tiempoTranscurrido;
 
+        //si el pajaro esta vivo y jugando
         if (state == VIVO) {
-            bird.actualizaPosicion();
-            boolean cambioNivel = false;
-            for (Usb usb : arriba) {
+            bird.actualizaPosicion(); //se actualiza su posicion en base a su velocidad
+            boolean cambioNivel = false; //indica si se incremento la dificultad
+            for (Usb usb : arriba) { //para cada usb, se actualiza su posicion y se incrementa el score si pasa al pajaro
                 usb.actualizaPosicion();
                 if (!usb.haIncrementadoScore() && usb.getPosX() + usb.getAncho() / 2 <= bird.getPosX() + bird.getAncho() / 2) {
                     usb.incrementarScore();
                     score++;
-                    if (score == 10 || score == 20) {
+                    if (score % 10 == 0) { //si en nivel es un multiplo de 10 se incrementa la dificultad
                         nivel++;
+                        //se actualizan las velocidades de los tubos
                         for (Usb usb2 : arriba) {
-                            usb2.setVelX(-getVelUsb(nivel));
+                            usb2.setVelX(-velUsb);
                         }
                         for (Usb usb2 : abajo) {
-                            usb2.setVelX(-getVelUsb(nivel));
+                            usb2.setVelX(-velUsb);
                         }
                     }
                 }
             }
-            if (cambioNivel) {
-                arriba.pollLast();
-                abajo.pollLast();
-            }
+
             for (Usb usb : abajo) {
-                usb.actualizaPosicion();
+                usb.actualizaPosicion(); //se actualizan las posiciones de los tubos en base a su velocidad
             }
+            //si debe saltar el pajaro, se incrementa su velocidad en y
             if (salta) {
                 salta = false;
                 bird.salta();
                 bird.setVelY(-velSalto);
             }
         } else if (state == MUERTO) {
-            bird.actualizaPosicion();
+            bird.actualizaPosicion(); //si esta muerto el pajaro y se presiono space o click, se restartea el juego
             if (restart) {
                 restart = false;
                 startGame();
@@ -245,12 +255,15 @@ public class Juego extends JFrame implements Runnable, KeyListener,
         }
 
     }
-    
+
+    //indica que se hace cuando muere el pajaro
     private void morir() {
-        state = MUERTO;
-        bird.setVelY(0);
-        nombre = JOptionPane.showInputDialog(this, "Ingresa tu nombre:", "");
-        try { //se intenta guardar
+        state = MUERTO; //se cambia el estado del juego a muerto
+        bird.setVelY(0); // se detiene el pajaro del salto
+        nombre = JOptionPane.showInputDialog(this, "Ingresa tu nombre:", ""); //se muestra un mensaje para escribir el nombre
+        
+        //se cargan los scores anteriores, se agrega el nuevo y se guarda todo
+        try {
             List<String> scores = cargar("src/flappy/saveFile.txt");
             scores.add(nombre + " - " + score);
             guardar("src/flappy/saveFile.txt", scores); // se guarda el juego
@@ -265,25 +278,32 @@ public class Juego extends JFrame implements Runnable, KeyListener,
      */
     public void checaColision() {
         if (state == VIVO) {
+            //colision del pajaron con la ventana
             if (bird.getPosY() + bird.getImageIcon().getIconHeight() >= getHeight() || bird.getPosY() <= 0) {
                 morir();
             }
+            
+            //si se sale el tubo de la ventana, se quita de la lista y se agrega otro al final
             if (arriba.peekFirst().getPosX() + arriba.peekFirst().getAncho() <= 0) {
-                insertaUsbs(getWidth() + separacionColumnas);
+                insertaUsbs(getWidth() + getSeparacionColumnas(nivel));
                 arriba.pollFirst();
                 abajo.pollFirst();
             }
+            
+            //interseccion de los tubos de arriba con el pajaro
             for (Usb usb : arriba) {
                 if (bird.intersecta(usb)) {
                     morir();
                 }
             }
+            //interseccion de los tubos de abajo con el pajaro
             for (Usb usb : abajo) {
                 if (bird.intersecta(usb)) {
                     morir();
                 }
             }
-        } else if (state == MUERTO) {
+        } else if (state == MUERTO) { //si el pajaro esta muerto
+            //colision del pajaro con el piso, para que se detenga el pajaro
             if (bird.getPosY() + bird.getImageIcon().getIconHeight() >= getHeight()) {
                 bird.setAccelY(0);
                 bird.setPosY(getHeight() - bird.getImageIcon().getIconHeight());
@@ -335,7 +355,7 @@ public class Juego extends JFrame implements Runnable, KeyListener,
                 salta = true;
             }
         }
-        if (e.getKeyCode() == KeyEvent.VK_P) {
+        if (e.getKeyCode() == KeyEvent.VK_P) { //se pausa el juego
             if (state < 3) {
                 state += PAUSA;
             } else if ((state & 4) != 0) {
@@ -379,14 +399,14 @@ public class Juego extends JFrame implements Runnable, KeyListener,
      * sobre la bola.
      */
     public void mousePressed(MouseEvent e) {
-        if (state == NO_EMPEZADO) {
+        if (state == NO_EMPEZADO) { //empieza a jugar
             state = VIVO;
             salta = true;
         } else if (state == VIVO) {
-            if (bird.getPosY() > 0) {
+            if (bird.getPosY() > 0) { //salta
                 salta = true;
             }
-        } else if (state == MUERTO) {
+        } else if (state == MUERTO) { //se restartea el juego
             restart = true;
         }
     }
@@ -472,37 +492,44 @@ public class Juego extends JFrame implements Runnable, KeyListener,
      */
     public void paint1(Graphics g) {
         g.clearRect(0, 0, getWidth(), getHeight());
+        //dibuja el background
         g.drawImage(BKG, 0, 0, null);
+        //dibuja al pajaro
         g.drawImage(bird.getImagenI(), (int) bird.getPosX(), (int) bird.getPosY(), null);
+        //dibuja los tubos de arriba
         for (Usb usb : arriba) {
             g.drawImage(usb.getImagenI(), (int) usb.getPosX(), (int) usb.getPosY(), null);
         }
 
+        //dibuja los tubos de abajo
         for (Usb usb : abajo) {
             g.drawImage(usb.getImagenI(), (int) usb.getPosX(), (int) usb.getPosY(), null);
         }
 
         g.setColor(Color.white);
         g.setFont(new Font("Arial", Font.BOLD, 36));
+        //escribe el score
         g.drawString(String.valueOf(score), getWidth() / 2 - 20, getHeight() / 3);
 
-        if (state == MUERTO) {
+        if (state == MUERTO) { //si esta muerto, se muestra un mensaje para volver a jugar
             g.drawString("Volver a jugar.", getWidth() / 2 - 120, getHeight() / 2);
         }
     }
 
+    //guarda los scores en el archivo
     private void guardar(String nombreArchivo, List<String> scores) throws IOException {
         File file = new File(nombreArchivo); // crea un objeto archivo a partir del nombre dado
         if (!file.exists()) { //si no existe el archivo, lo crea
             file.createNewFile();
         }
         try (PrintWriter writer = new PrintWriter(file, "UTF-8")) { //crea un escritor que se autocierra
-            for(String score : scores) {
+            for(String score : scores) { //guarda cada score de la lista
                 writer.println(score);
             }
         }
     }
 
+    //carga los scores de la lista
     private List<String> cargar(String nombreArchivo) throws FileNotFoundException {
         File file = new File(nombreArchivo);
         if (!file.exists()) { //si no existe el archivo
@@ -513,7 +540,7 @@ public class Juego extends JFrame implements Runnable, KeyListener,
 
         try (Scanner scanner = new Scanner(file)) { //crea un lector que se autocierra
             while(scanner.hasNextLine()) {
-                scores.add(scanner.nextLine());
+                scores.add(scanner.nextLine()); //pone el score en la lista
             }
         }
         
